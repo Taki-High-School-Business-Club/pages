5183e9b3448c8e7d7a98d8b9c88d1affeb6321b0
coko-go <89577404+coko-go@users.noreply.github.com>
2021-11-26 22:23:44 +0900

    change

--- make_html/js/makehtml.js
+++ make_html/js/makehtml.js
@@ -2,7 +2,18 @@
 //Global
 let result = "";
 //function
-function reqYt(url) {
+function AddBr(sen){
+  const spIn = sen.split("\n");
+  const spIn_len = spIn.length;
+  console.log(spIn_len);
+  let output = "";
+  for (let i = 0; i < spIn_len - 1; ++i) {
+    output += spIn[i] + "<br>";
+  }
+  output += spIn[spIn_len - 1];
+  return output;
+}
+function ReqYt(url) {
   let ytCode;
   let ytID;
   try {
@@ -27,7 +38,7 @@ function reqYt(url) {
   }
   return ytCode;
 }
-function reqAct(ac0, ac1, ac2) {
+function ReqAct(ac0, ac1, ac2) {
   let res_active;
   try {
     if (ac2.length != 0) {
@@ -55,26 +66,36 @@ function reqAct(ac0, ac1, ac2) {
   }
   return res_active;
 }
-function reqImp(imp0, imp1) {
+function ReqImp(imp0, imp1) {
   let res_imp;
+  let imp_sen = "";
+  imp0 = AddBr(imp0);
+  imp1 = AddBr(imp1);
   try {
-    if (imp1.length != 0) {
-      res_imp = `<fieldset style="border:1px solid #7d7d7d;font-size:100%;padding:20px 20px 0 20px;margin:10px 0;"><legend class="strong">今回の感想</legend>\
-            <ul><li><!--感想1-->${imp0}</li><li><!--感想2-->${imp1}</li></ul>`;
-    } else if (imp0.length != 0) {
-      res_imp = `<fieldset style="border:1px solid #7d7d7d;font-size:100%;padding:20px 20px 0 20px;margin:10px 0;"><legend class="strong">今回の感想</legend>\
-            <ul><li><!--感想1-->${imp0}</li></ul></fieldset>`;
+    if (imp0.length != 0 && imp1.length !=0) {
+      imp_sen = `<li><!--感想1-->${imp0}</li><li><!--感想2-->${imp1}</li>`;
+    } else if (imp0.length != 0 || imp1.length !=0) {
+      let temp_imp;
+      let count_imp;
+      if(imp0.length > imp1.length){
+        count_imp = 1;
+        temp_imp = imp0;
+      }else{
+        count_imp = 2;
+        temp_imp = imp1;
+      }
+      imp_sen = `<li><!--感想${count_imp}-->${temp_imp}</li>`;
     } else {
-      res_imp = "";
       result += "No Impressions \n";
     }
+    res_imp = `<fieldset style="border:1px solid #7d7d7d;font-size:100%;padding:20px 20px 0 20px;margin:10px 0;"><legend class="strong">今回の感想</legend><ul>${imp_sen}</ul>`;
   } catch (error) {
     alert("多分感想の欄を間違えてるんじゃあないかな?");
     return;
   }
   return res_imp;
 }
-function getTime() {
+function GetTime() {
   var nowDate = new Date();
   var year = nowDate.getFullYear();
   var month = nowDate.getMonth() + 1;
@@ -84,6 +105,10 @@ function getTime() {
   var seconds = nowDate.getSeconds();
   var outDate = `<${year}-${month}-${date} ${hours}:${minutes}:${seconds} UTC+9:00>`;
   return outDate;
+}
+function MainSentence(sen) {
+  let outSen = AddBr(sen);
+  return outSen;
 }
 $(document).ready(function () {
   //makehtml
@@ -96,9 +121,9 @@ $(document).ready(function () {
     let main;
     let ytUrl;
     //実行時間
-    let outTime = getTime();
+    let outTime = GetTime();
     //入力
-    main = $("#main").val();
+    main = MainSentence($("#main").val());
     active[0] = $("#active0").val();
     active[1] = $("#active1").val();
     active[2] = $("#active2").val();
@@ -113,11 +138,11 @@ $(document).ready(function () {
     //Main Sentenceの処理
     let res_main = `<p><!--本文-->${main}</p>`;
     //activeの処理
-    let res_active = reqAct(active[0], active[1], active[2]);
+    let res_active = ReqAct(active[0], active[1], active[2]);
     //impressの処理
-    let res_imp = reqImp(impress[0], impress[1]);
+    let res_imp = ReqImp(impress[0], impress[1]);
     //ytの処理
-    ytCode = reqYt(ytUrl);
+    ytCode = ReqYt(ytUrl);
     //Responseまとめる
     const res = `${res_main}${res_active}${res_imp}<div style="text-align :right;"><p style="font-family: 'Lato', sans-serif;">Taki High School Business Club</p></div>${ytCode}`;
     //Output
