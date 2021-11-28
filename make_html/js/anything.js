03d963bde5e6ed55c9c2f067941da8559d8e454b
coko-go <89577404+coko-go@users.noreply.github.com>
2021-11-28 20:08:24 +0900

    2.2.

--- make_html/js/anything.js
+++ make_html/js/anything.js
@@ -8,3 +8,20 @@ $(document).ready(function () {
     $(".copyright").text(`2021-${y} coko-go`);
   }
 });
+//スクロール
+window.addEventListener('DOMContentLoaded', () => {
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+  anchorLinksArr.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const targetId = link.hash;
+      const targetElement = document.querySelector(targetId);
+      const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top;
+      window.scrollTo({
+        top: targetOffsetTop,
+        behavior: "smooth"
+      });
+    });
+  });
+});
