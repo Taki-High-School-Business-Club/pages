2f9f428ff2ab4064081a38873d1a99df68746407
coko-go <89577404+coko-go@users.noreply.github.com>
2021-12-05 16:21:15 +0900

    2.3

--- make_html/js/makehtml.js
+++ make_html/js/makehtml.js
@@ -1,6 +1,7 @@
 //bugs
 //Global
 let result = "";
+let resMediaType = "";
 //function
 function AddBr(sen){
   const spIn = sen.split("\n");
@@ -12,31 +13,6 @@ function AddBr(sen){
   }
   output += spIn[spIn_len - 1];
   return output;
-}
-function ReqYt(url) {
-  let ytCode;
-  let ytID;
-  try {
-    let spInput = url.split("/");
-    //let ytID;
-    if (spInput[2] == "www.youtube.com") {
-      console.log("normal");
-      const spInput2 = spInput[3].split("=");
-      ytID = spInput2[1];
-      ytCode = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${ytID}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-    } else if (spInput[2] == "youtu.be" || spInput[2] == "y2u.be") {
-      console.log("min-url");
-      ytID = spInput[3];
-      ytCode = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${ytID}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-    } else {
-      console.log("Not Right URL");
-      result += "No YouTube";
-      ytCode = "";
-    }
-  } catch (error) {
-    alert("YouTubeの例外エラー");
-  }
-  return ytCode;
 }
 function ReqAct(ac0, ac1, ac2) {
   let res_active;
@@ -110,16 +86,89 @@ function MainSentence(sen) {
   let outSen = AddBr(sen);
   return outSen;
 }
+function Media(type,url){
+  if(type == "yt"){
+    mediaCode = ReqYt(url);
+  }else if(type == "picture"){
+    mediaCode = ReqPic(url);
+  }else if(type == "movie"){
+    mediaCode = ReqMov(url);
+  }else if(type == "music"){
+    mediaCode = ReqMusic(url);
+  }
+  function ReqYt(url) {
+    let ytCode;
+    let ytID;
+    try {
+      let spInput = url.split("/");
+      //let ytID;
+      if (spInput[2] == "www.youtube.com") {
+        console.log("normal");
+        const spInput2 = spInput[3].split("=");
+        ytID = spInput2[1];
+        ytCode = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${ytID}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+        resMediaType = "Youtube";
+      } else if (spInput[2] == "youtu.be" || spInput[2] == "y2u.be") {
+        console.log("min-url");
+        ytID = spInput[3];
+        ytCode = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${ytID}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+        resMediaType = "Youtube";
+      } else {
+        result += "No Media";
+        ytCode = "";
+      }
+    } catch (error) {
+      alert("YouTubeの例外エラー");
+    }
+    return ytCode;
+  }
+  function ReqPic(url){
+    let picCode;
+    if(url.length !=0 ){
+      picCode = `<img src="${url}">`;
+      resMediaType = "Picture";
+    }else{
+      result += "No Media";
+      picCode = "";
+    }
+    return picCode;
+  }
+  function ReqMov(url){
+    let movCode;
+    if(url.length !=0 ){
+      movCode = `<video controls><source src="${url}"></video>`;
+      resMediaType = "Movie";
+    }else{
+      result += "No Media";
+      movCode = "";
+    }
+    return movCode;
+  }
+  function ReqMusic(url){
+    let musicCode;
+    if(url.length !=0 ){
+      musicCode = `<audio controls src="${url}">Your browser does not support the<code>audio</code> element.</audio>`;
+      resMediaType = "Music";
+    }else{
+      result += "No Media";
+      musicCode = "";
+    }
+    return musicCode;
+  }
+  return mediaCode;
+}
 $(document).ready(function () {
   //makehtml
   $(".button button").on("click", function () {
     //初期化
     result = "";
+    resMediaType = "";
     //宣言
     const active = [];
     const impress = [];
     let main;
-    let ytUrl;
+    let mediaUrl;
+    let sortMedia;
     //実行時間
     let outTime = GetTime();
     //入力
@@ -129,33 +178,36 @@ $(document).ready(function () {
     active[2] = $("#active2").val();
     impress[0] = $("#impress0").val();
     impress[1] = $("#impress1").val();
+    sortMedia = $("#chooseMedia").val();
+    mediaUrl = $("#media").val();
     //本文必須化
     if (main.length == 0) {
       alert("本文を入力してね");
       return;
     }
-    ytUrl = $("#yt").val();
     //Main Sentenceの処理
     let res_main = `<p><!--本文-->${main}</p>`;
     //activeの処理
     let res_active = ReqAct(active[0], active[1], active[2]);
     //impressの処理
     let res_imp = ReqImp(impress[0], impress[1]);
-    //ytの処理
-    ytCode = ReqYt(ytUrl);
+    //mediaの処理
+    let res_media = Media(sortMedia,mediaUrl);
     //Responseまとめる
-    const res = `${res_main}${res_active}${res_imp}<div style="text-align :right;"><p style="font-family: 'Lato', sans-serif;">Taki High School Business Club</p></div>${ytCode}`;
+    const res = `${res_main}${res_active}${res_imp}<div style="text-align :right;"><p style="font-family: 'Lato', sans-serif;">Taki High School Business Club</p></div>${res_media}`;
     //Output
     $(".output").text(res);
     //console
     $(".result").append(
-      `<p>${outTime}</p><p class="end">ALL PROCESSES FINISHED<br><span class="no-con">${result}</span></p>`
+      `<p>${outTime}</p><p class="end">ALL PROCESSES FINISHED<br><span class="no-con">${result}</span><br><span class="show-type">Media Type:${resMediaType}</span></p>`
     );
   });
-  //YouTube-Preview
-  $("#yt-bt").on("click", function () {
-    const ytUrl = $("#yt").val();
-    let ytCode = reqYt(ytUrl);
-    $("#yt-pre").html(ytCode);
+  //Media-Preview
+  $("#media-pre-bt").on("click", function () {
+    let sortMedia = $("#chooseMedia").val();
+    window.console.log(sortMedia);
+    const ytUrl = $("#media").val();
+    let mediaCode = Media(sortMedia,ytUrl);
+    $("#media-pre").html(mediaCode);
   });
 });
