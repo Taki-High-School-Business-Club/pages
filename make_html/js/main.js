c84d8e1fae94e7c364b34c976fdf0a66fd0dfa87
coko-go <cokogo53@gmail.com>
2022-05-25 22:38:53 +0900

    20220525

--- make_html/js/main.js
+++ make_html/js/main.js
@@ -1,6 +1,7 @@
 //宣言
 let mainSen;
 mainSen = document.getElementById('main');
+let makeHtmlButton = document.getElementById('make-html');
 //Common_function
 function makeIDString(num){
     if(num < 10){
@@ -11,7 +12,64 @@ function makeIDString(num){
     }
 }
 // * Active用のfunction類
-let actC = 3;
+let actC = 3; //初期値
+function increaseActiveButton(){
+    console.log('Increase!!')
+    actC++;
+    increaseForm(actC,"act");
+    console.log(`actC = ${actC}`);
+    console.log("ID="+makeIDString(actC));
+}
+function decreaseActiveButton(){
+    if(actC > 1){
+        actC--;
+        console.log('Decrease!!');
+        decreaseForm(actC,"act");
+    }else if(actC == 1){
+        console.log('Not Decrease!!');
+    }else if(actC < 0){
+        console.log('Exception : c < 0');
+    }
+    console.log(`actC = ${actC}`);
+    console.log("ID="+makeIDString(actC));
+}
+// * Impressionのfunction類　
+let impC = 2; // 初期値
+function increaseImpressButton(){
+    console.log('Increase!!')
+    impC++;
+    increaseForm(impC,"imp");
+    console.log(`impC = ${impC}`);
+    console.log("ID="+makeIDString(impC));
+}
+function decreaseImpressButton(){
+    if(impC > 1){
+        impC--;
+        console.log('Decrease!!');
+        decreaseForm(impC,"imp");
+    }else if(impC == 1){
+        console.log('Not Decrease!!');
+    }else if(impC < 0){
+        console.log('Exception : c < 0');
+    }
+    console.log(`impC = ${impC}`);
+    console.log("ID="+makeIDString(impC));
+}
+
+// * Form 変更
+function increaseForm(id,mode){
+    //mode: Active = act , Impress = imp;
+    let a = document.getElementById(`li-${mode + makeIDString(id - 1)}`);
+    let nowID = makeIDString(id);
+    let label = mode == "act" ? "活動内容" : "今回の感想";
+    let htmlCode = mode == "act" ? `<input type="text" placeholder="${label + nowID}" id="${ mode + nowID}">` : `<textarea type="text" placeholder="${label + nowID}" id="${ mode + nowID}"></textarea>`;
+    a.insertAdjacentHTML('afterend',`<li id="${"li-"+ mode + nowID}"><label for="active0">${label + nowID}</label><br>${htmlCode}</li>`);
+}
+function decreaseForm(id,mode){
+    //mode: Active = act , Impress = imp;
+    let a = document.getElementById(`li-${mode + makeIDString(id + 1)}`);
+    a.remove();
+}
 function increaseActiveForm(id){
     var a = document.getElementById("li-act" + makeIDString(id-1));
     var nowID = makeIDString(id)
@@ -21,28 +79,6 @@ function decreaseActiveForm(id){
     var a = document.getElementById("li-act" + makeIDString(id + 1));
     a.remove();
 }
-function increaseActiveButton(){
-    console.log('Increase!!')
-    actC++;
-    increaseActiveForm(actC);
-    console.log(`actC = ${actC}`);
-    console.log("ID="+makeIDString(actC));
-}
-function decreaseActiveButton(){
-    if(actC > 1){
-        actC--;
-        console.log('Decrease!!');
-        decreaseActiveForm(actC);
-    }else if(actC == 1){
-        console.log('Not Decrease!!');
-    }else if(actC < 0){
-        console.log('Exception : c < 0');
-    }
-    console.log(`actC = ${actC}`);
-    console.log("ID="+makeIDString(actC));
-}
-// * Impressionのfunction類
-let impC = 2;
 function increaseImpressForm(id){
     var a = document.getElementById("li-imp" + makeIDString(id-1));
     var nowID = makeIDString(id)
@@ -52,32 +88,23 @@ function decreaseImpressForm(id){
     var a = document.getElementById("li-imp" + makeIDString(id + 1));
     a.remove();
 }
-function increaseImpressButton(){
-    console.log('Increase!!')
-    impC++;
-    increaseImpressForm(impC);
-    console.log(`impC = ${impC}`);
-    console.log("ID="+makeIDString(impC));
-}
-function decreaseImpressButton(){
-    if(impC > 1){
-        impC--;
-        console.log('Decrease!!');
-        decreaseImpressForm(impC);
-    }else if(impC == 1){
-        console.log('Not Decrease!!');
-    }else if(impC < 0){
-        console.log('Exception : c < 0');
-    }
-    console.log(`impC = ${impC}`);
-    console.log("ID="+makeIDString(impC));
-}
+
 //イベント
 window.onload = function(){
-    document.getElementById('make-html').onclick = function(){
+    document.getElementById('main').addEventListener("change",function(){
+        console.log("Change action");
+        console.log(this.value);
+        if(this.value == false){
+            console.log("it's empty!!!");
+            makeHtmlButton.disabled = true;
+        }else{
+            makeHtmlButton.disabled = false;
+        }
+      });
+    makeHtmlButton.onclick = function(){
         //alert("clicked!");
-        mainSen = document.getElementById('main');
-        alert(mainSen.value)
+        //mainSen = document.getElementById('main');
+        //alert(mainSen.value)
     }
     //Active Increase and Decrease;
     document.getElementById('active-increase').onclick = increaseActiveButton;
